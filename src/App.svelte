0acb8eaf0b731ef0733c840a4b69ac09bca25da7
yesnoruly <karvatskij.roma@gmail.com>
2022-08-29 21:24:23 +0300

    feat: add styles

--- src/App.svelte
+++ src/App.svelte
@@ -1,12 +1,9 @@
 <script>
     import Table from "./lib/Table.svelte";
+    import Diagram from "./lib/Diagram.svelte";
 
     let text = '';
     let words = [];
-
-    function loadSample() {
-        text = '"Anything that happens, happens. Anything that, in happening, causes something else to happen, causes something else to happen. Anything that, in happening, causes itself to happen again, happens again. It doesn\'t necessarily do it in chronological order, though."'
-    }
 
     function countWords() {
         if (text) {
@@ -17,7 +14,6 @@
                 .replace(/\s+/g, " ") // Replace empty lines, tabs, etc. with space
                 .split(' ');
 
-            // Filter repeating words in array
             let wordsFiltered = [...new Set(words)];
 
             // Generate array with objects
@@ -30,16 +26,71 @@
                 .sort((firstWord, secondWord) => secondWord.num - firstWord.num);
             }
     }
+
+    function loadSample() {
+        text = '"Anything that happens, happens. Anything that, in happening, causes something else to happen, causes something else to happen. Anything that, in happening, causes itself to happen again, happens again. It doesn\'t necessarily do it in chronological order, though."'
+    }
 </script>
 
 <div class="wf">
-    <textarea bind:value={text} id="textarea-freq" placeholder="Enter text"></textarea>
-    <button on:click={countWords}>Count words</button>
-    <button on:click={loadSample}>Load sample</button>
+    <textarea bind:value={text} placeholder="Enter text"></textarea>
+    <button class="btn-filled" on:click={countWords}>Count words</button>
+    <button class="btn-empty" on:click={loadSample}>Load sample</button>
 </div>
 
-<Table words={words}/>
+{#if words.length}
+    <Table words={words}/>
+    <Diagram />
+{/if}
+
+
 
 <style>
+    .wf {
+        background-color: #fff;
+        padding: 35px 25px 25px;
+        grid-area: wf;
+    }
 
+    textarea {
+
+        font-family: 'Roboto Mono', sans-serif;
+        width: 100%;
+        min-height: 249px;
+        resize: none;
+        padding: 15px;
+
+        background: #FFFFFF;
+        border: 2px solid rgba(13, 107, 196, 0.6);
+        border-radius: 2px;
+        font-size: 14px;
+        line-height: 21px;
+        color: #023E96;
+    }
+
+    button {
+        margin: 25px 25px 0 0;
+
+        height: 40px;
+        min-width: 130px;
+        border-radius: 10px;
+        text-align: center;
+    }
+
+    button:last-child {
+        margin: 25px 0 0;
+    }
+
+    .btn-filled {
+        background: #258CF4;
+        color: #fff;
+        border: none;
+    }
+
+    .btn-empty {
+        background: #FFFFFF;
+        border: 3px solid #258CF4;
+        border-radius: 10px;
+        color: #258CF4;
+    }
 </style>
--- src/lib/Table.svelte
+++ src/lib/Table.svelte
@@ -2,26 +2,72 @@
     export let words;
 </script>
 
-<table>
-    <thead>
+<div>
+    <table>
+        <thead>
         <tr>
             <th colspan="1">Word</th>
             <th colspan="1">Frequency</th>
         </tr>
-    </thead>
+        </thead>
 
-    <tbody>
+        <tbody>
         {#each words as {word, num}}
             <tr>
                 <td colspan="1">{word}</td>
                 <td colspan="1">{num}</td>
             </tr>
         {/each}
-    </tbody>
-</table>
+        </tbody>
+    </table>
+</div>
 
 <style>
+    div {
+        grid-area: table;
+        padding: 25px;
+
+        background: #fff;
+    }
+
     table {
         text-align: center;
+        border-collapse: collapse;
+
+        width: 100%;
+        height: 100%;
+    }
+
+    thead {
+        background: #258CF4;
+        border-bottom: 3px solid #0D6BC4;
+    }
+
+    th {
+        font-family: Roboto, sans-serif;
+        font-size: 18px;
+        text-transform: uppercase;
+        color: #FFFFFF;
+        padding: 10px;
+        letter-spacing: 3px;
+
+        width: 150px;
+    }
+
+    tr {
+        font-family: Roboto, sans-serif;
+        font-size: 14px;
+        color: #000;
+        line-height: 34px;
+        letter-spacing: 1px;
+        max-width: 150px;
+        word-break: break-word;
+    }
+
+    tr:nth-child(odd) {
+        background: rgba(37, 140, 244, 0.7);
+    }
+    tr:nth-child(even) {
+        background: rgba(37, 140, 244, 0.2);
     }
 </style>
