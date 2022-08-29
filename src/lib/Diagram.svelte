d4cb6c9063d61cc0fda638448fb8aec6817d0116
yesnoruly <karvatskij.roma@gmail.com>
2022-08-29 21:25:06 +0300

    feat: add diagram section

---
+++ src/lib/Diagram.svelte
@@ -0,0 +1,20 @@
+<script>
+
+</script>
+
+<div>Diagram soon...</div>
+
+<style>
+    div {
+        grid-area: diagram;
+        background-color: #fff;
+        display: grid;
+        justify-content: center;
+        padding-top: 50px;
+
+        font-size: 30px;
+        line-height: 28px;
+
+        color: #072240;
+    }
+</style>
