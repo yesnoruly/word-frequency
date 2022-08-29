2bd3e2daf1652da72457da918e93e3e69dded7fe
yesnoruly <karvatskij.roma@gmail.com>
2022-08-29 15:32:18 +0300

    feat: add table

---
+++ src/lib/Table.svelte
@@ -0,0 +1,27 @@
+<script>
+    export let words;
+</script>
+
+<table>
+    <thead>
+        <tr>
+            <th colspan="1">Word</th>
+            <th colspan="1">Frequency</th>
+        </tr>
+    </thead>
+
+    <tbody>
+        {#each words as {word, num}}
+            <tr>
+                <td colspan="1">{word}</td>
+                <td colspan="1">{num}</td>
+            </tr>
+        {/each}
+    </tbody>
+</table>
+
+<style>
+    table {
+        text-align: center;
+    }
+</style>
