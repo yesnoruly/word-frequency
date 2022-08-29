f6aacccd4c72408671465cfbab626fb6e685a236
yesnoruly <karvatskij.roma@gmail.com>
2022-08-29 21:43:54 +0300

    chore: move input into lib

--- src/App.svelte
+++ src/App.svelte
@@ -1,6 +1,7 @@
 <script>
     import Table from "./lib/Table.svelte";
     import Diagram from "./lib/Diagram.svelte";
+    import Input from "./lib/Input.svelte";
 
     let text = '';
     let words = [];
@@ -32,65 +33,9 @@
     }
 </script>
 
-<div class="wf">
-    <textarea bind:value={text} placeholder="Enter text"></textarea>
-    <button class="btn-filled" on:click={countWords}>Count words</button>
-    <button class="btn-empty" on:click={loadSample}>Load sample</button>
-</div>
+<Input value={text} countWords={countWords} loadSample={loadSample} />
 
 {#if words.length}
     <Table words={words}/>
     <Diagram />
 {/if}
-
-
-
-<style>
-    .wf {
-        background-color: #fff;
-        padding: 35px 25px 25px;
-        grid-area: wf;
-    }
-
-    textarea {
-
-        font-family: 'Roboto Mono', sans-serif;
-        width: 100%;
-        min-height: 249px;
-        resize: none;
-        padding: 15px;
-
-        background: #FFFFFF;
-        border: 2px solid rgba(13, 107, 196, 0.6);
-        border-radius: 2px;
-        font-size: 14px;
-        line-height: 21px;
-        color: #023E96;
-    }
-
-    button {
-        margin: 25px 25px 0 0;
-
-        height: 40px;
-        min-width: 130px;
-        border-radius: 10px;
-        text-align: center;
-    }
-
-    button:last-child {
-        margin: 25px 0 0;
-    }
-
-    .btn-filled {
-        background: #258CF4;
-        color: #fff;
-        border: none;
-    }
-
-    .btn-empty {
-        background: #FFFFFF;
-        border: 3px solid #258CF4;
-        border-radius: 10px;
-        color: #258CF4;
-    }
-</style>
---
+++ src/lib/Input.svelte
@@ -0,0 +1,61 @@
+<script>
+    export let value;
+    export let countWords;
+    export let loadSample;
+</script>
+
+<div class="input">
+    <textarea bind:value={value} placeholder="Enter text"></textarea>
+    <button class="btn-filled" on:click={countWords}>Count words</button>
+    <button class="btn-empty" on:click={loadSample}>Load sample</button>
+</div>
+
+<style>
+    .input {
+        background-color: #fff;
+        padding: 35px 25px 25px;
+        grid-area: input;
+    }
+
+    textarea {
+
+        font-family: 'Roboto Mono', sans-serif;
+        width: 100%;
+        min-height: 249px;
+        resize: none;
+        padding: 15px;
+
+        background: #FFFFFF;
+        border: 2px solid rgba(13, 107, 196, 0.6);
+        border-radius: 2px;
+        font-size: 14px;
+        line-height: 21px;
+        color: #023E96;
+    }
+
+    button {
+        margin: 25px 25px 0 0;
+
+        height: 40px;
+        min-width: 130px;
+        border-radius: 10px;
+        text-align: center;
+    }
+
+    button:last-child {
+        margin: 25px 0 0;
+    }
+
+    .btn-filled {
+        background: #258CF4;
+        color: #fff;
+        border: none;
+    }
+
+    .btn-empty {
+        background: #FFFFFF;
+        border: 3px solid #258CF4;
+        border-radius: 10px;
+        color: #258CF4;
+    }
+</style>
